046dec0506f9c07ad2c471e0e9167c450bb73eaa
Cesar <cesar777welch@gmail.com>
2018-11-19 14:07:58 -0600

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SessionComponent } from './session/session.component';
 import { SessionviewerComponent } from './sessionviewer/sessionviewer.component';
-import { QuestionsComponent } from './Questions/questions.component';
+
 
 
 const routes: Routes = [{
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { QuestionsComponent } from './questions/questions.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SessionviewerComponent } from './sessionviewer/sessionviewer.component';
 import { SessionComponent } from './session/session.component';
@@ -14,7 +13,6 @@ import { FormsModule }   from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent,
     NavbarComponent,
     SessionviewerComponent,
     SessionComponent
